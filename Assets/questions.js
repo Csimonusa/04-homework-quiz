766f408d8429ce78e2d9873c2f51892d3cbfa306
mona <csimonusa@outlook.com>
2022-01-03 18:18:39 -0500

    changing approach after seeing dan's method in office hours. added seperate js file for my questions and removed from html

---
+++ Assets/questions.js
@@ -0,0 +1,48 @@
+let questions = [
+    {
+        number: 1,
+        question: "Who was the first buccaneer to be inducted into the hall of fame?",
+        answer: "lee roy selmon",
+        choices: [
+            "lee roy selmon",
+            "john lynch",
+            "ron wolf",
+            "tony dungy"
+        ]
+    },
+    {
+        number: 2,
+        question: "the tampa bay buccaneers won super bowl xxxvii over what team?",
+        answer: "oakland raiders",
+        choices: [
+            "oakland raiders",
+            "miami dolphins",
+            "cleaveland browns",
+            "tony dungy"
+        ]
+    },
+    {
+        number: 3,
+        question: "the tampa bay buccaneers won superbowl lv over what team?",
+        answer: "kansas city chiefs",
+        choices: [
+            "kansas city chiefs",
+            "indianapolis colts",
+            "tennessee titans",
+            "houston texans"
+        ]
+    },
+    {
+        number: 4,
+        question: "what division do the buccaneers play in?",
+        answer: "nfc south",
+        choices: [
+            "nfc south",
+            "nfc north",
+            "afc south",
+            "afc north"
+        ]
+    },
+    
+]
+

100d7555796ff5b99df7193f15d1d2d5a3e9de55
mona <csimonusa@outlook.com>
2022-01-05 16:14:03 -0500

    user can now save score and initials to local storage

--- Assets/script.js
+++ Assets/script.js
@@ -2,6 +2,7 @@ var time = 30
 var timer = document.getElementById("time")
 var timerId
 var startbtn = document.querySelector(".start-button")
+var submitbtn = document.getElementById("submit")
 var wc1 = document.querySelector(".wrong")
 var wc2 = document.querySelector(".wrong1")
 var wc3 = document.querySelector(".wrong2")
@@ -20,6 +21,9 @@ var correctTwo = document.getElementById("correct2")
 var correctThree = document.getElementById("correct3")
 var correctFour = document.getElementById("correct4")
 var score = document.getElementById("score")
+var initalsEl = document.getElementById("initials")
+var nextquest = document.querySelectorAll("choice")
+// var scoreList = document.querySelector(".score-list")
 
 function start() {
     var startQuiz = document.getElementById("intro")
@@ -81,6 +85,23 @@ function nextQuestion() {
  }
 
  correct.addEventListener("click", next)
+ wc1.addEventListener("click", next)
+ wc2.addEventListener("click", next)
+ wc3.addEventListener("click", next)
+ wc4.addEventListener("click", nextTwo)
+ wc5.addEventListener("click", nextTwo)
+ wc6.addEventListener("click", nextTwo)
+ wc7.addEventListener("click", nextThree)
+ wc8.addEventListener("click", nextThree)
+ wc9.addEventListener("click", nextThree)
+ wc10.addEventListener("click", end)
+ wc11.addEventListener("click", end)
+ wc12.addEventListener("click", end)
+
+
+
+ 
+
 
  function nextTwo() {
     var q3 = document.getElementById("q3")
@@ -107,6 +128,45 @@ function nextQuestion() {
     questions.setAttribute("id", "hide")
     clearInterval(timerId)
     score.textContent = time
+    submitbtn.addEventListener("click", function(e) {
+       e.preventDefault()
+       var getLocalStrigified = localStorage.getItem("highscoreArr") || '[]'
+       var highScoreArr = JSON.parse(getLocalStrigified)
+       var hiscore = time
+       var userInitials = initalsEl.value
+       var hiScoreObj = {
+          initials: userInitials,
+          score: hiscore
+       }
+       highScoreArr.push(hiScoreObj)
+       var stringifiedArr = JSON.stringify(highScoreArr)
+       localStorage.setItem("highscoreArr", stringifiedArr)
+       clearForm()
+   })
+ }
+
+ function clearForm() {
+    document.getElementById("initials").value= ""
  }
 
  correctFour.addEventListener("click", end)
+ 
+//  function scorer() {
+//    var scoreList = document.querySelector(".score-list")
+//    scoreList.setAttribute("id", "show1")
+//    var questions = document.querySelector(".questions4")
+//    questions.setAttribute("id", "hide")
+//    var ending = document.querySelector(".ender")
+//    ending.setAttribute("id", "hide")
+//    var questions = document.querySelector(".questions3")
+//    questions.setAttribute("id", "hide")
+//    var questions = document.querySelector(".questions2")
+//    questions.setAttribute("id", "hide")
+//    var questions = document.querySelector(".questions")
+//    questions.setAttribute("id", "hide")
+//    var showIntro = document.getElementById("intro")
+//    showIntro.setAttribute("id", "hide")
+//  }
+
+//  scoreList.addEventListener("click", scorer)
+
